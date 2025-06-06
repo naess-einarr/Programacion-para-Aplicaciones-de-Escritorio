7352c5ba2bdbd5755c3cf2a2a8d2f152b2e57ab9
MZSM98 <marcoszenon98@gmail.com>
2025-06-06 02:25:09 -0600

    Login cambios

--- src/main/java/com/eurobank/proyectoaplicacionesdeescritorio/controlador/LoginController.java
+++ src/main/java/com/eurobank/proyectoaplicacionesdeescritorio/controlador/LoginController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -32,11 +33,15 @@ public class LoginController implements Initializable {
         @FXML
         private TextField textUsuario;
         
+        @FXML
+        private Button botonIniciarSesion;
+        
         private EmpleadoDAO empleadoDAO;
 
         @Override
         public void initialize(URL url, ResourceBundle rb) {
             empleadoDAO = new EmpleadoDAO();
+            botonIniciarSesion.setDefaultButton(true);
         }
 
         @FXML

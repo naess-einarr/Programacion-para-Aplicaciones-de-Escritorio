d425cf5684ec37840e2e8fc51792203cdd92521c
MZSM98 <marcoszenon98@gmail.com>
2025-06-09 11:19:39 -0600

    Cambios menu

--- src/main/java/com/eurobank/proyectoaplicacionesdeescritorio/controlador/MenuController.java
+++ src/main/java/com/eurobank/proyectoaplicacionesdeescritorio/controlador/MenuController.java
@@ -1,7 +1,9 @@
 package com.eurobank.proyectoaplicacionesdeescritorio.controlador;
 
 import com.eurobank.proyectoaplicacionesdeescritorio.modelo.Empleado;
+import com.eurobank.proyectoaplicacionesdeescritorio.modelo.Gerente;
 import com.eurobank.proyectoaplicacionesdeescritorio.modelo.Sucursal;
+import com.eurobank.proyectoaplicacionesdeescritorio.util.EmpleadoDatosUtil;
 import com.eurobank.proyectoaplicacionesdeescritorio.vista.ManejadorDeSesion;
 import com.eurobank.proyectoaplicacionesdeescritorio.vista.ManejadorDeVistas;
 import java.net.URL;
@@ -9,11 +11,26 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class MenuController implements Initializable {
-    
+    @FXML
+    private Button botonAdministrarClientes;
+
+    @FXML
+    private Button botonAdministrarCuentasBancarias;
+
+    @FXML
+    private Button botonAdministrarEmpleados;
+
+    @FXML
+    private Button botonAdministrarSucursales;
+
+    @FXML
+    private Button botonAdministrarTransacciones;
+
     @FXML
     private Label textNombre;
     @FXML
@@ -25,6 +42,7 @@ public class MenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         if(ManejadorDeSesion.haySesionActiva()){
             cargarDatosMenuEmpleado();
+            ajustarMenuPerfil();
         }
     }
     
@@ -68,4 +86,25 @@ public class MenuController implements Initializable {
         String sucursalActual= sucursal != null ? sucursal.toString() : "Sin sucursal asignada";
         textSucursal.setText(sucursalActual);
     }
+    
+private void ajustarMenuPerfil(){
+    Empleado empleado = ManejadorDeSesion.obtenerEmpleado();
+    String tipoEmpleado = empleado.getTipoEmpleado();
+    
+    if(EmpleadoDatosUtil.TIPO_GERENTE.equals(tipoEmpleado)){
+        if(empleado instanceof Gerente){
+           botonAdministrarSucursales.setDisable(!EmpleadoDatosUtil.NIVEL_NACIONAL.equals(((Gerente) empleado).getNivelAcceso())); // Gerente puede administrar sucursales
+        }
+    } else if(EmpleadoDatosUtil.TIPO_EJECUTIVO.equals(tipoEmpleado)){
+        botonAdministrarTransacciones.setDisable(true);
+        botonAdministrarSucursales.setDisable(true);
+        botonAdministrarEmpleados.setDisable(true);
+    } else if(EmpleadoDatosUtil.TIPO_CAJERO.equals(tipoEmpleado)){
+        botonAdministrarClientes.setDisable(true);
+        botonAdministrarCuentasBancarias.setDisable(true);
+        botonAdministrarSucursales.setDisable(true);
+        botonAdministrarEmpleados.setDisable(true);
+        botonAdministrarTransacciones.setDisable(ManejadorDeSesion.getSucursalActual()==null);
+    }
+}
 }

176a7f3790979103098099b416cea80aa7ec89e6
MZSM98 <marcoszenon98@gmail.com>
2025-06-09 03:31:29 -0600

    logica de transacciones

--- src/main/java/com/eurobank/proyectoaplicacionesdeescritorio/controlador/TransaccionRegistroController.java
+++ src/main/java/com/eurobank/proyectoaplicacionesdeescritorio/controlador/TransaccionRegistroController.java
@@ -1,7 +1,6 @@
 package com.eurobank.proyectoaplicacionesdeescritorio.controlador;
 
 import com.eurobank.proyectoaplicacionesdeescritorio.dao.CuentaDAO;
-import com.eurobank.proyectoaplicacionesdeescritorio.dao.SucursalDAO;
 import com.eurobank.proyectoaplicacionesdeescritorio.dao.TransaccionDAO;
 import com.eurobank.proyectoaplicacionesdeescritorio.modelo.Cuenta;
 import com.eurobank.proyectoaplicacionesdeescritorio.modelo.Empleado;
@@ -9,10 +8,10 @@ import com.eurobank.proyectoaplicacionesdeescritorio.modelo.Sucursal;
 import com.eurobank.proyectoaplicacionesdeescritorio.util.AlertaUtil;
 import com.eurobank.proyectoaplicacionesdeescritorio.util.ConstantesUtil;
 import com.eurobank.proyectoaplicacionesdeescritorio.util.TransaccionDatosUtil;
+import com.eurobank.proyectoaplicacionesdeescritorio.vista.ManejadorDeSesion;
 import com.eurobank.proyectoaplicacionesdeescritorio.vista.ManejadorDeVistas;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.UUID;
 import javafx.collections.FXCollections;
@@ -45,14 +44,12 @@ public class TransaccionRegistroController implements Initializable {
     
     CuentaDAO cuentabancariaDAO;
     TransaccionDAO transaccionDAO;
-    SucursalDAO sucursalDAO;
     Sucursal sucursal;
     Empleado empleadoActual;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        sucursalDAO = new SucursalDAO();
         cuentabancariaDAO = new CuentaDAO();
         transaccionDAO = new TransaccionDAO();
         comboTipoTransaccion.setItems(TransaccionDatosUtil.listaTipoTransaccion());
@@ -99,31 +96,24 @@ public class TransaccionRegistroController implements Initializable {
     private void obtenerEmpleadoActual() {
         try {
             MenuController menuController = ManejadorDeVistas.getInstancia().obtenerControlador(ManejadorDeVistas.Vista.MENU);
-            if (menuController != null) {
-//                this.empleadoActual = menuController.getEmpleado();
-            }
         } catch (Exception ex) {
             LOG.error("Error al obtener empleado actual", ex);
         }
     }
     
-    private void cargarSucursalEmpleado() {
-        
+        private void cargarSucursalEmpleado() {
         try {
-            List<Sucursal> todasLasSucursales = sucursalDAO.obtenerTodos();
+            if (ManejadorDeSesion.haySesionActiva()) {
+                Sucursal sucursalActual = ManejadorDeSesion.getSucursalActual();
 
-            for (Sucursal sucursal : todasLasSucursales) {
-                for (Empleado empleado : sucursal.getEmpleadosAsociados()) {
-                    if (empleado.getIdEmpleado().equals(empleadoActual.getIdEmpleado())) {
-                        this.sucursal = sucursal;
-                        textSucursal.setText(sucursal.toString());
-                        return;
-                    }
+                if (sucursalActual != null) {
+                    textSucursal.setText(sucursalActual.toString());
                 }
             }
         } catch (Exception ex) {
             LOG.error("Error al cargar sucursal del empleado", ex);
         }
     }
+
     
 }
